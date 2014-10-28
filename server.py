6c8cc91851585c2e9bd0e71ea0d69b0608b58d45
grant mclendon <grant.mclendon@gmail.com>
2014-10-28 13:34:21 +0000

    updated to upstream

--- server.py
+++ server.py
@@ -133,5 +133,5 @@ api.add_resource(HelpRequestAsJSON, '/request/<string:helprequest_id>.json')
 
 # start the server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
 
