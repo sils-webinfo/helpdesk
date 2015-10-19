0d763bd777ea7779be635294b05f4b3e5bbb136b
Ryan Shaw <ryanshaw@unc.edu>
2015-10-19 10:45:28 -0400

    turn on debugging

--- server.py
+++ server.py
@@ -137,4 +137,4 @@ api.add_resource(HelpRequestAsJSON, '/request/<string:helprequest_id>.json')
 
 # start the server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+    app.run(host='0.0.0.0', port=5555, debug=True)
