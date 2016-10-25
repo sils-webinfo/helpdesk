c357b7dd60c8ad65c29d40c4d20786fc62037156
rybesh <ryanshaw@unc.edu>
2016-10-25 13:34:36 +0000

    Change default port to 8888

--- server.py
+++ server.py
@@ -197,4 +197,4 @@ def after_request(response):
 
 # Start the server.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
